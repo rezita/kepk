0f5655ca374a37a99193b8503d685ddb435b6742
unknown <zita.reiner@gmail.com>
2021-09-01 14:43:34 +0100

    frontend: thumbnail and gallery disply changes

--- gallery.js
+++ gallery.js
@@ -1,9 +1,12 @@
+const thumbsCont = document.getElementById("thumbs-main");
+const nrOfImages = 15; //nr of half of the images in thumbs container -1
+
 function loadItem(catalog, idx) {
     let main = document.getElementById("main");
     let item = catalog[idx];
     if (item.type == "img") {
         let img = document.createElement("img");
-        img.classList.add("content");
+        img.classList.add("main-image");
         img.src = item.src;
         let orient = item.orient
         switch (orient) {
@@ -24,7 +27,7 @@ function loadItem(catalog, idx) {
         let vid = document.createElement("video");
         vid.src = item.src;
         vid.controls = true;
-        vid.classList.add("content");
+        vid.classList.add("main-image");
         vid.classList.add("rotate0");
         main.replaceChild(vid, main.childNodes[0]);
     }
@@ -45,6 +48,7 @@ function loadItem(catalog, idx) {
             loadItem(catalog, idx + 1);
         }
     };
+    loadThumbnails(catalog, idx);
 }
 
 function init() {
@@ -56,5 +60,113 @@ function init() {
             loadItem(jsonCatalog, 0);
         })
 }
+
+function loadThumbnails(catalog, idx) {
+    if (thumbsCont == null) {
+        return false;
+    }
+    //remove previous images from thumbs container
+    while (thumbsCont.lastChild) {
+        thumbsCont.removeChild(thumbsCont.lastChild);
+    }
+
+    let catalogLength = catalog.length;
+    let firsIdx = calculateIndex(idx, - nrOfImages, catalogLength);
+    let lastIdx = calculateIndex(idx, nrOfImages, catalogLength);
+
+    for (i = - nrOfImages; i <= nrOfImages; i++) {
+        if (i == 0) {
+            addMediaToThumbs(catalog, idx, i, true);
+        } else {
+            addMediaToThumbs(catalog, idx, i);
+        }
+    }
+
+    document.getElementById("thumbs-prev").onclick = function () {
+        thumbsCont.removeChild(thumbsCont.lastChild);
+        lastIdx = calculateIndex(lastIdx, -1, catalogLength);
+        firsIdx = calculateIndex(firsIdx, +1, catalogLength);
+        let imgContainer = createImageContainer(false);
+        thumbsCont.insertBefore(imgContainer, thumbsCont.firstChild);
+        loadMdiaItem(imgContainer, catalog, firsIdx);
+    };
+
+    document.getElementById("thumbs-next").onclick = function () {
+        thumbsCont.removeChild(thumbsCont.firstChild);
+        lastIdx = calculateIndex(lastIdx, +1, catalogLength);
+        firsIdx = calculateIndex(firsIdx, -1, catalogLength);
+        addMediaToThumbs(catalog, lastIdx, 0, false);
+    };
+
+    thumbsCont.style.width = calculateWidthForContainer(nrOfImages, 100) + "px";
+    thumbsCont.style.marginLeft = calculateMarginLeft(nrOfImages, 100) + "px";
+}
+
+function calculateIndex(originalIndex, step, dataLength) {
+    result = (originalIndex + step + dataLength) % dataLength;
+    if (result < 0) { //in case if there are less images then nrOfImages
+        result += dataLength;
+    } 
+    return result;
+}
+
+function addMediaToThumbs(catalog, idx, step, selected) {
+    let imgContainer = createImageContainer(selected)
+    thumbsCont.appendChild(imgContainer);
+
+    let cIdx = calculateIndex(idx, step, catalog.length);
+    loadMdiaItem(imgContainer, catalog, cIdx);
+}
+
+function createImageContainer(selected) {
+    let imgCont = document.createElement('div');
+    imgCont.classList.add('thumbs-div');
+    if (selected) {
+        imgCont.classList.add('selected');
+    }
+    return imgCont;
+}
+
+function loadMdiaItem(parentContainer, catalog, index) {
+    let cItem = catalog[index];
+    let cImg = document.createElement("img");
+    if (cItem.hasOwnProperty("thumbnail")) {
+/*        fetch(cItem.thumbnail)
+        .then(res => {
+            if (res.ok) {
+                cImg.src = cItem.thumbnail;
+            } else {
+                cImg.src = "noThumbnail.jpg";
+            }
+        }).catch(err => cImg.src = "noThumbnail.jpg");*/
+        cImg.src = cItem.thumbnail;
+        //if the image can not be loaded
+        cImg.addEventListener("error", function(event) {
+            event.target.src = "noThumbnail.jpg";
+            event.onerror = null;
+        });
+    } else {
+        cImg.src = "noThumbnail.jpg";
+    }
+    cImg.onclick = function () {
+        loadItem(catalog, index);
+    }
+    
+    parentContainer.appendChild(cImg);
+}
+
+function calculateWidthForContainer(nrOfImages, imageWidth) {
+    return (2 * nrOfImages + 1) * imageWidth;
+}
+
+function calculateMarginLeft(nrOfImages, imageWidth) {
+    let cWidth = calculateWidthForContainer(nrOfImages, imageWidth);
+    let visibleWidth = document.getElementById("thumbs").offsetWidth;
+    //console.log(visibleWidth);
+    let marginLeftValue = Math.round(- (cWidth - visibleWidth + 3 * imageWidth) / 2);
+    //console.log(marginLeftValue);
+    return marginLeftValue;
+}
+
 
 init();
